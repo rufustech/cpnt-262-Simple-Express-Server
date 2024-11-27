51827c19825c8198cf56c45de0655d5abbf67030
rmucheri <vamucheri@gmail.com>
2024-11-26 19:34:05 -0700

    fix 404 route

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.use("/users", usersRouter);
 
 // 404 Handler
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "pages", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 // Error Handler
 app.use(errorHandler);
 
 // Start Server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
